0c1351e32fa403e11de6b4d421486de5c89e80d5
Sanders Lee <slee_colonel@hotmail.com>
2015-04-04 23:11:37 -0700

    Lab 08 nearly done.

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,5 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+define('ROLE_USER','user');
+define('ROLE_ADMIN','admin');
+
 // location of data folders
 define('DATAPATH', './data/');
 define('XMLSUFFIX','.xml');
